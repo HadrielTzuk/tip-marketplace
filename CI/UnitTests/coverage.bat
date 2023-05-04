53a749b2be5401330d3d2f8bc0dd1acb261478e5
Hadriel Tzuk <73405001+HadrielTzuk@users.noreply.github.com>
2023-05-04 15:23:10 +0300

    Delete CI directory

--- CI/UnitTests/coverage.bat
+++
@@ -1,5 +0,0 @@
-echo "Launching new coverage report"
-coverage run -m pytest
-coverage report
-coverage html
-echo "HTML version is available in ./htmlcov/index.html"
--- CI/UnitTests/coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Launching new coverage report"
-coverage run -m pytest
-coverage report
-coverage html
-echo "HTML version is available in ./htmlcov/index.html"
